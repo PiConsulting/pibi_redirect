8b728e5d31feafae9662c94c934be88d7942c8ff
lautarodriguez7 <lautarodriguez17@gmail.com>
2023-11-29 16:07:56 -0300

    changing config for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default {
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxInject: `import React from 'react';`,
-  },
-  esbuildOptions: {
-    loader: {
-      '.js': 'jsx',
-    },
-  },
-};
+export default defineConfig({
+  plugins: [react()],
+  base: "/PiConsulting/pibi_redirect/",
+});
